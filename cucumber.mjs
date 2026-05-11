0d2977ac6648551c025325b4add7b123d9bdabbc
Geozedeque Guimaraes <GeozedequeGuimaraes@users.noreply.github.com>
2026-05-10 23:08:18 -0300

    test: valida execucao dos cenarios bdd

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,6 +1,8 @@
-export default {
+const config = {
   paths: ["tests/bdd/features/**/*.feature"],
   import: ["tests/bdd/steps/**/*.mjs"],
   format: ["progress"],
   publishQuiet: true,
 };
+
+export default config;
